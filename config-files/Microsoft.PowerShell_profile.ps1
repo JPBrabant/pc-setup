c2466d3ecf2a52086c34745df6165b41d3c46fa1
Jean-Philippe Brabant <JPBrabant@outlook.com>
2025-11-01 10:53:09 -0400

    Initial commit

---
+++ config-files/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+oh-my-posh init pwsh --config 'catppuccin' | Invoke-Expression
+
+Import-Module -Name posh-git                # Auto-complete for Git
+Import-Module -Name Microsoft.WinGet.Client # PowerShell style wrapper for WinGet
+Import-Module -Name Terminal-Icons          # Icons for files with Get-ChildItems
+
+# Network Utilities
+function Get-IP { 
+    $externalIP = Invoke-WebRequest -Uri 'http://ifconfig.me/ip'
+    $localIP = Get-NetIPAddress -AddressFamily IPv4 `
+                | Where-Object -FilterScript { $_.AddressState -eq 'Preferred' -and $_.IPAddress -ne '127.0.0.1' } `
+                | Sort-Object -Property InterfaceIndex `
+                | Select-Object -First 1
+
+    $ip = @{
+        Local = $localIP.IPAddress
+        External = $externalIP.Content 
+    }
+
+    return $ip
+}
+
+# Desktop
+function Set-DesktopLocation { Set-Location -Path ( [Environment]::GetFolderPath('Desktop') ) }
