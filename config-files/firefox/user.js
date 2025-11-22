2dd4e66935ee418513756b7cfff3ce35e41e2562
Jean-Philippe Brabant <JPBrabant@outlook.com>
2025-11-22 16:43:11 -0500

    Adding settings about the window title bar and the side bar in my Firefox settings file

--- config-files/firefox/user.js
+++ config-files/firefox/user.js
@@ -3,8 +3,19 @@
 // Do NOT edit `prefs.js` directly while Firefox is running; instead
 // modify this file to persist preferences across restarts.
 
-// Restore previous windows and tabs on startup (UI: "Open previous windows and tabs").
-// 0 = blank page, 1 = homepage, 3 = restore previous session
+// What is shown at startup
+// 0 = blank page, 1 = home page (default), 2 = last visited page, 3 = resume the previous browser session
+// https://kb.mozillazine.org/Browser.startup.page
 user_pref("browser.startup.page", 3);
 
-// End of user.js
+// Show the Window Title Bar (draw the tab over the title bar area)
+// 0 = Show title bar, 1 = Hide title bar (default)
+// https://searchfox.org/firefox-release/source/modules/libpref/init/StaticPrefList.yaml#1929
+user_pref("browser.tabs.inTitlebar", 0);
+
+// New side bar with tab on the side
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.verticalTabs.dragToPinPromo.dismissed", true);
+user_pref("sidebar.main.tools", "history,bookmarks");
+user_pref("sidebar.visibility", "always-show");
