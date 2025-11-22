5339dddba3d6b05a07fd4882d4614a85d96a4a92
Jean-Philippe Brabant <JPBrabant@outlook.com>
2025-11-22 10:48:54 -0500

    Adding my Firefox profile

---
+++ config-files/firefox/user.js
@@ -0,0 +1,10 @@
+// user.js
+// This file sets preferences that are applied at Firefox startup.
+// Do NOT edit `prefs.js` directly while Firefox is running; instead
+// modify this file to persist preferences across restarts.
+
+// Restore previous windows and tabs on startup (UI: "Open previous windows and tabs").
+// 0 = blank page, 1 = homepage, 3 = restore previous session
+user_pref("browser.startup.page", 3);
+
+// End of user.js
